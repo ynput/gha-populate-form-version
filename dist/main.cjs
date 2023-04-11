8db0ebbdb1c6c805c51a9d84cf0fd8fccc7ee2e7
Jakub Jezek <jakubjezek001@gmail.com>
2023-04-11 16:56:38 +0200

    fixing tag name

--- dist/main.cjs
+++ dist/main.cjs
@@ -89820,7 +89820,7 @@ async function listGithubReleases(repoName) {
 			await github
 				.getOctokit(process.env.GITHUB_TOKEN)
 				.rest.repos.listTags({ owner, repo, per_page: 100, page })
-		).data.map((value) => value.tag_name.replace(/^CI\//g, ''));
+		).data.map((value) => value.name.replace(/^CI\//g, ''));
 		tags.push(...results);
 		page++;
 		return results.length > 0 && fetch();
